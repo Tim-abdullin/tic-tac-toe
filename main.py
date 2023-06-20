7eca6f3149ca52867f6475ca7bfbb31083b3aa53
Tim-abdullin <tim_abdullin@mail.ru>
2023-06-20 16:52:30 +0300

    Added the ability to select a marker for the player

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ def print_board():
     print('_____________')
     print('| ' + board[6] + ' | ' + board[7] + ' | ' + board[8] + ' |')
     print('_____________')
+
+
+def choose_marker():
+    global player1_marker, player2_marker
+    while player1_marker not in ('X', 'O'):
+        player1_marker = input('Игрок 1, выберете маркер Х или О: ').upper()
+        if player1_marker not in ('X', 'O'):
+            print('Выберете Х или О (eng)')
+        if player1_marker == 'X':
+            player2_marker = 'O'
+        else:
+            player2_marker = 'X'
