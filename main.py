8c9c4e6580dfb31e92b947bc24413289ba9233ea
Tim-abdullin <tim_abdullin@mail.ru>
2023-06-20 18:11:25 +0300

    Added functions of random selection of who goes first and the algorithm of the player's move

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 board = [' ' for i in range(9)]     # создаем пустую доску для игры
 player1_marker = None   # маркер 1 игрока
 player2_marker = None   # маркер 2 игрока
@@ -24,3 +27,30 @@ def choose_marker():
             player2_marker = 'O'
         else:
             player2_marker = 'X'
+
+
+def first_player():
+    global current_player
+    if random.randint(0, 1) == 0:
+        current_player = 'player_1'
+    else:
+        current_player = 'player_2'
+    print(f'{current_player} ходит первым')
+
+
+def player_move():
+    global current_player, board
+    while True:
+        move = input(f'{current_player}, введите номер ячейки для хода (1-9): ')
+        if not move.isdigit() or int(move) < 1 or int(move) > 9:
+            print('Некорректный ввод. Выберете номер ячейки от 1 до 9')
+            continue
+        move = int(move) - 1
+        if board[move] != ' ':
+            print('Эта ячейка уже занята')
+        else:
+            if current_player == 'player_1':
+                board[move] = player1_marker
+            else:
+                board[move] = player2_marker
+            break
